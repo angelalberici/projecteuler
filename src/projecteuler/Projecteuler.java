c98400caf2a574099526162b32f0925b94c7d503
angel_000 <angel_000@AngelLaptop>
2015-06-30 11:55:00 -0430

    Skeleton

---
+++ src/projecteuler/Projecteuler.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projecteuler;
+
+/**
+ *
+ * @author angel_000
+ */
+public class Projecteuler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //multiplesOf3And5();
+        
+        
+    }
+    
+    
+    public static void multiplesOf3And5(){
+   /* int limit = 1000;
+    
+        for (int i = 0; i < limit; i++) {
+            
+        }
+    */
+    
+    }
+    
+}
