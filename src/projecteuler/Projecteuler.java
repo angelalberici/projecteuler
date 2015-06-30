99f5c2dd34245786e21e23093f4a2515a00b362f
angel_000 <angel_000@AngelLaptop>
2015-06-30 12:30:20 -0430

    First 2 exercises

--- src/projecteuler/Projecteuler.java
+++ src/projecteuler/Projecteuler.java
@@ -1,35 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package projecteuler;
 
-/**
+/** I will be solving the exercises from the ProjectEuler in this Java Project
  *
- * @author angel_000
+ * @author Angel Alberici
  */
 public class Projecteuler {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //multiplesOf3And5();
-        
+        int n = 2;
+        switch(n){
+                case 1: multiplesOf3And5();
+                    break;
+                case 2: evenFibonacciNumbers();
+                    break;
+        }
         
     }
     
     
     public static void multiplesOf3And5(){
-   /* int limit = 1000;
-    
+    int limit = 1000;
+    int sum = 0;
+        
         for (int i = 0; i < limit; i++) {
-            
+        if(i % 3 == 0 || i % 5 == 0)
+            sum = sum + i;
         }
-    */
+        System.out.println("The defined limit: "+ limit);
+        System.out.println("Sum Total: " + sum);
     
+    }
+    
+    public static void  evenFibonacciNumbers(){
+        Integer limit = 4000000; // 4000000
+        int first = 1;
+        int second = 2;
+        
+        int nextNumber = 0;
+        int valueA = first;
+        int valueB = second;
+        int sum = 2; //It starts with 1,2; we have a first even number (TWO)
+        
+        while(nextNumber<limit){
+            nextNumber = valueA + valueB;
+            valueA= valueB;
+            valueB = nextNumber;
+            
+            if( nextNumber % 2 == 0)
+                sum = sum + nextNumber;
+        }
+        System.out.println("The defined limit: "+ limit);
+        System.out.println("Sum Total: " + sum);
+        
     }
     
 }
