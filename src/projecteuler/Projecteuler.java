e16eb52b7b606c862917591895e2c632137b7b67
Angel Alberici <angelalberici@gmail.com>
2017-09-04 17:26:50 +0100

    Merge into one class with one method per problem Minor fixes

--- src/projecteuler/Projecteuler.java
+++ src/projecteuler/Projecteuler.java
@@ -6,8 +6,40 @@ package projecteuler;
  */
 public class Projecteuler {
     public static void main(String[] args) {
-                 MultiplesOf3And5.multiplesOf3And5();
-                EvenFibonacciNumbers.evenFibonacciNumbers();
+                 multiplesOf3And5();
+                evenFibonacciNumbers();
     }
     
+        public static void  evenFibonacciNumbers(){
+        Integer limit = 4000000; // 4000000
+        int valueA = 1;
+        int valueB = 2;
+        int sum = 2; //It starts with 1,2; we have a first even number (TWO)
+        int nextNumber = 0;
+        
+        while(nextNumber<limit){
+            nextNumber = valueA + valueB;
+            valueA= valueB;
+            valueB = nextNumber;
+            
+            if( nextNumber % 2 == 0)
+                sum = sum + nextNumber;
+        }
+        System.out.println("The defined limit: "+ limit);
+        System.out.println("Sum Total: " + sum);
+        
+    }
+        
+            public static void multiplesOf3And5() {
+        int limit = 1000;
+        int sum = 0;
+        for (int i = 0; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println("The defined limit: " + limit);
+        System.out.println("Sum Total: " + sum);
+
+    }
 }
